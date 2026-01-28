bab1b1701cda9ac7ecfa9550567a20946e6ed2c0
osmangonirabbi13 <osmangoniyou12@gmail.com>
2026-01-28 21:00:54 +0600

    login page basic setup

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,41 @@
+import { LoginForm } from "@/components/layouts/authentication/login-form"
+import { House } from 'lucide-react';
+import Image from "next/image"
+import Link from "next/link"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link href="/" className="flex items-center gap-2 font-medium">
+            <div className=" flex size-6 items-center justify-center">
+            </div>
+            <button>
+                <div className="flex justify-center gap-2  cursor-pointer outline-1 p-4  hover:bg-black hover:text-amber-50 dark:hover:bg-white dark:hover:text-black">
+                    <div><House/></div>
+                <div>Back To Home</div>
+                </div>
+                
+            </button>
+            
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <Image
+          src="/login.png"
+          alt="Image"
+          fill
+         priority
+          className="absolute inset-0  object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h1>This is register page </h1>
+        </div>
+    );
+};
+
+export default RegisterPage;
